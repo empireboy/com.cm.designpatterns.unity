1f08c9b27fb365b2e12cf060649714a3b388b880
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2020-06-23 20:26:09 +0200

    Added CommandSO, a ScriptableObject that represents a Command

---
+++ Command/Scripts/Runtime/CommandSO.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace CM.Patterns.Command
+{
+	/// <summary>
+	/// Represents a reference to a Command as a ScriptableObject.
+	/// </summary>
+	public abstract class CommandSO : ScriptableObject
+	{
+		/// <summary>
+		/// Gets the Command that this ScriptableObject represents.
+		/// </summary>
+		/// <returns>The Command that this ScriptableObject represents.</returns>
+		public abstract Command GetCommand();
+	}
+}
